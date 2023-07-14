b109f4b0e11de5b15b7c102ff1c1df60d8c77098
jtimilli <65932886+jtimilli@users.noreply.github.com>
2023-07-14 00:23:48 -0400

    Add fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/game-grid";
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
       <Show above="lg">
         <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main">Main</GridItem>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
     </Grid>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+dotenv.config();
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+
+    define: {
+      GAMES_API_KEY: JSON.stringify({ VITE_API_KEY: env.VITE_API_KEY }),
+    },
+  };
+});
